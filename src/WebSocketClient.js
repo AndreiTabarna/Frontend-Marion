91707cf8415fd05a45a7164eceb00b86968799ff
AndreiTabarna <100340254+AndreiTabarna@users.noreply.github.com>
2024-01-08 19:44:43 +0200

    Update WebSocketClient.js

--- src/WebSocketClient.js
+++ src/WebSocketClient.js
@@ -1,40 +1,35 @@
 // WebSocketClient.js
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 const WebSocketClient = () => {
-  const [socket, setSocket] = useState(null);
+  const socketRef = useRef(null);
 
-  useEffect(() => {
-    // Dummy WebSocket connection
-    const newSocket = new WebSocket('wss://frontend-marion-production.up.railway.app:3001/ws');
+  const connectWebSocket = () => {
+    const socket = new WebSocket('wss://frontend-marion-production.up.railway.app:3001/ws');
 
-    // Dummy event listeners (you can customize as needed)
-    newSocket.addEventListener('open', (event) => {
+    socket.addEventListener('open', (event) => {
       console.log('WebSocket connection opened:', event);
-      setSocket(newSocket);
     });
 
-    newSocket.addEventListener('message', (event) => {
+    socket.addEventListener('message', (event) => {
       console.log('WebSocket message received:', event.data);
       // Handle incoming messages as needed
     });
 
-    newSocket.addEventListener('close', (event) => {
-      console.log('WebSocket connection closed:', event);
+    // Note: No 'close' event listener
 
-      // Attempt to reconnect on close
-      setTimeout(() => {
-        console.log('Attempting to reconnect...');
-        newSocket.close();
-      }, 1000);
-    });
+    socketRef.current = socket;
+  };
+
+  useEffect(() => {
+    connectWebSocket();
 
     // Do not close the connection on unmount
     console.log('WebSocketClient component mounted');
 
     // Cleanup function
-    // Note: We are not closing the connection here to avoid the error
+    // Note: No need to close the connection on unmount
     return () => {
       console.log('Cleanup: WebSocketClient component unmounted');
       // No need to close the connection here
