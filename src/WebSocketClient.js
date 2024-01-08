26f74f8687053af6082243a0ec3daad9c053cfd0
AndreiTabarna <100340254+AndreiTabarna@users.noreply.github.com>
2024-01-08 19:50:44 +0200

    socket

--- src/WebSocketClient.js
+++ src/WebSocketClient.js
@@ -1,13 +1,23 @@
 // WebSocketClient.js
 
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
+import ReconnectingWebSocket from 'reconnecting-websocket';
 
 const WebSocketClient = () => {
-  const socketRef = useRef(null);
+  useEffect(() => {
+    // Configure WebSocket options
+    const options = {
+      WebSocket: ReconnectingWebSocket, // Use the reconnecting-websocket library
+      connectionTimeout: 1000,
+      maxRetries: Infinity,
+      maxReconnectionDelay: 4000,
+      minReconnectionDelay: 1000,
+    };
 
-  const connectWebSocket = () => {
-    const socket = new WebSocket('wss://frontend-marion-production.up.railway.app:3001/ws');
+    // Dummy WebSocket connection
+    const socket = new ReconnectingWebSocket('wss://frontend-marion-production.up.railway.app:3001/ws', [], options);
 
+    // Dummy event listeners (you can customize as needed)
     socket.addEventListener('open', (event) => {
       console.log('WebSocket connection opened:', event);
     });
@@ -17,22 +27,18 @@ const WebSocketClient = () => {
       // Handle incoming messages as needed
     });
 
-    // Note: No 'close' event listener
-
-    socketRef.current = socket;
-  };
-
-  useEffect(() => {
-    connectWebSocket();
+    socket.addEventListener('close', (event) => {
+      console.log('WebSocket connection closed:', event);
+    });
 
     // Do not close the connection on unmount
     console.log('WebSocketClient component mounted');
 
     // Cleanup function
-    // Note: No need to close the connection on unmount
+    // Note: Close the connection on unmount
     return () => {
       console.log('Cleanup: WebSocketClient component unmounted');
-      // No need to close the connection here
+      socket.close();
     };
   }, []);
 
