3f940c32dd6015675f516edff215b7fcf7e39bc0
AndreiTabarna <100340254+AndreiTabarna@users.noreply.github.com>
2024-01-08 21:40:26 +0200

    Update WebSocketClient.js

--- src/WebSocketClient.js
+++ src/WebSocketClient.js
@@ -6,43 +6,33 @@ let socket;
 
 const WebSocketClient = () => {
   useEffect(() => {
+    // Establish WebSocket connection
+    socket = new WebSocket('wss://frontend-marion-production.up.railway.app:3001/ws');
+
     // Dummy event listeners (you can customize as needed)
-    const handleWebSocketClose = (event) => {
+    socket.addEventListener('open', (event) => {
+      console.log('WebSocket connection opened:', event);
+    });
+
+    socket.addEventListener('message', (event) => {
+      console.log('WebSocket message received:', event.data);
+      // Handle incoming messages as needed
+    });
+
+    socket.addEventListener('close', (event) => {
       console.log('WebSocket connection closed:', event);
-      // Attempt to reconnect after a delay if needed
+      // Attempt to reconnect after a delay
       setTimeout(() => {
-        establishWebSocketConnection();
+        console.log('Reconnecting...');
+        socket = new WebSocket('wss://frontend-marion-production.up.railway.app:3001/ws');
       }, 2000);
-    };
-
-    const establishWebSocketConnection = () => {
-      // Dummy WebSocket connection
-      socket = new WebSocket('wss://frontend-marion-production.up.railway.app:3001/ws');
-
-      // Dummy event listeners (you can customize as needed)
-      socket.addEventListener('open', (event) => {
-        console.log('WebSocket connection opened:', event);
-      });
-
-      socket.addEventListener('message', (event) => {
-        console.log('WebSocket message received:', event.data);
-        // Handle incoming messages as needed
-      });
-
-      socket.addEventListener('close', handleWebSocketClose);
-    };
-
-    // Establish WebSocket connection after a 2-second delay
-    const connectionTimeout = setTimeout(() => {
-      establishWebSocketConnection();
-    }, 2000);
+    });
 
     // Cleanup function
     return () => {
       console.log('Cleanup: WebSocketClient component unmounted');
-      clearTimeout(connectionTimeout); // Clear the timeout if the component is unmounted
-      if (socket) {
-        socket.removeEventListener('close', handleWebSocketClose);
+      if (socket && socket.readyState === WebSocket.OPEN) {
+        socket.close();
       }
     };
   }, []);
