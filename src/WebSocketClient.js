d9332ac71bfd327d0ebed104a5eacbd481cede0e
AndreiTabarna <100340254+AndreiTabarna@users.noreply.github.com>
2024-01-08 20:33:16 +0200

    Update WebSocketClient.js

--- src/WebSocketClient.js
+++ src/WebSocketClient.js
@@ -1,37 +1,35 @@
 // WebSocketClient.js
 
 import React, { useEffect } from 'react';
-import ReconnectingWebSocket from 'reconnecting-websocket';
 
 const WebSocketClient = () => {
   useEffect(() => {
-    // Configure WebSocket options
-    const options = {
-      WebSocket: ReconnectingWebSocket,
-      connectionTimeout: 1000,
-      maxRetries: Infinity,
-      maxReconnectionDelay: 4000,
-      minReconnectionDelay: 1000,
-      reconnectionDelayGrowFactor: 1.3, // Adjust this value as needed
-      connectionTimeout: 10000, // Increase the connection timeout if needed
+    let socket;
+
+    const connectWebSocket = () => {
+      socket = new WebSocket('wss://frontend-marion-production.up.railway.app:3001/ws');
+
+      socket.addEventListener('open', (event) => {
+        console.log('WebSocket connection opened:', event);
+      });
+
+      socket.addEventListener('message', (event) => {
+        console.log('WebSocket message received:', event.data);
+        // Handle incoming messages as needed
+      });
+
+      socket.addEventListener('close', (event) => {
+        console.log('WebSocket connection closed:', event);
+
+        // Reconnect on close with a timeout
+        setTimeout(() => {
+          connectWebSocket();
+        }, 10000);
+      });
     };
 
-    // Dummy WebSocket connection
-    const socket = new ReconnectingWebSocket('wss://frontend-marion-production.up.railway.app:3001/ws', [], options);
-
-    // Dummy event listeners (you can customize as needed)
-    socket.addEventListener('open', (event) => {
-      console.log('WebSocket connection opened:', event);
-    });
-
-    socket.addEventListener('message', (event) => {
-      console.log('WebSocket message received:', event.data);
-      // Handle incoming messages as needed
-    });
-
-    socket.addEventListener('close', (event) => {
-      console.log('WebSocket connection closed:', event);
-    });
+    // Initial connection
+    connectWebSocket();
 
     // Do not close the connection on unmount
     console.log('WebSocketClient component mounted');
@@ -40,7 +38,9 @@ const WebSocketClient = () => {
     // Note: Close the connection on unmount
     return () => {
       console.log('Cleanup: WebSocketClient component unmounted');
-      socket.close();
+      if (socket) {
+        socket.close();
+      }
     };
   }, []);
 
